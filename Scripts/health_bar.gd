ec63c05bb8a8319f2b39ba667402dcd72d0c45d5
Darius <beldi_darius@yahoo.com>
2025-05-12 12:48:25 +0300

    updated healthbar
    rewritten the healthbar and added it to the gui

--- Scripts/health_bar.gd
+++ Scripts/health_bar.gd
@@ -1,20 +1,12 @@
-extends ProgressBar
+extends TextureProgressBar
 
 func _ready():
-	max_value = 300
-	value = 0 
+	pass
 
-	await get_tree().process_frame
+func set_max_health(_max_health):
+	max_value = _max_health 
+	update_health(_max_health)
 
-	var health_components = get_tree().get_nodes_in_group("health_components")
-	
-	if health_components.size() > 0:
-		var health_component = health_components[0]
-		health_component.connect("health_changed", Callable(self, "_on_health_changed"))
-	else:
-		print("ERROR: Didn't find HealthComponent!")
-
-func _on_health_changed(new_health, max_health):
-	self.max_value = max_health
-	self.value = new_health
-	print("Health Bar has changed: ", new_health, "/", max_health)
+func update_health(_current_health):
+	value = _current_health
+	$HealthLabel.text = str(_current_health) + "/" + str(int(max_value))
