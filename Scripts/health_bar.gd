f481a49e7419a11dbe6b2435da10a2c7b3256a22
Luca <164880454+imnofuxkingfun@users.noreply.github.com>
2025-05-13 00:43:38 +0300

    added animations for mc and collisions and new dungeon design (it has some bugs)
    bugs - starting place teleportation and ??? particles dissapear in village when too close

--- Scripts/health_bar.gd
+++ Scripts/health_bar.gd
@@ -6,15 +6,15 @@ func _ready():
 
 	await get_tree().process_frame
 
-	var health_components = get_tree().get_nodes_in_group("health_components")
-	
-	if health_components.size() > 0:
-		var health_component = health_components[0]
-		health_component.connect("health_changed", Callable(self, "_on_health_changed"))
-	else:
-		print("ERROR: Didn't find HealthComponent!")
-
-func _on_health_changed(new_health, max_health):
-	self.max_value = max_health
-	self.value = new_health
-	print("Health Bar has changed: ", new_health, "/", max_health)
+	#var health_components = get_tree().get_nodes_in_group("health_components")
+	#
+	#if health_components.size() > 0:
+		#var health_component = health_components[0]
+		#health_component.connect("health_changed", Callable(self, "_on_health_changed"))
+	#else:
+		#print("ERROR: Didn't find HealthComponent!")
+#
+#func _on_health_changed(new_health, max_health):
+	#self.max_value = max_health
+	#self.value = new_health
+	#print("Health Bar has changed: ", new_health, "/", max_health)
