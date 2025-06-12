3b4823c8e6a380ac13a83182f4b40b73851c3a9d
Mihai Danaila <slendyman35@gmail.com>
2025-06-12 15:29:16 +0300

    Added attack cooldown

--- Scenes/Attack/attack.gd
+++ Scenes/Attack/attack.gd
@@ -2,7 +2,13 @@ extends Node2D
 class_name Attack
 
 @export var attackDamage = 10.0
-@export var attackCooldown = 1.0 # secunde
+@export var attackCooldown = 1000 # ms
 
-func attack(directionX, directionY, startPosition: Vector2):
+var canAttack: bool = true
+
+func attackLogic(directionX, directionY, startPosition: Vector2):
 	push_error("attack() not implemented")
+	
+func attack(directionX, directionY, startPosition: Vector2):
+	if canAttack:
+		attackLogic(directionX, directionY, startPosition)
