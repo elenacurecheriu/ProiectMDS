77b559124df4bb670fe4eb46f6adb8e3266164ee
Luca <164880454+imnofuxkingfun@users.noreply.github.com>
2025-06-14 04:46:20 +0300

    added village1,2,3, panda room, cat room scenes

--- Scripts/transition/to_village_2.gd
+++ Scripts/transition/to_village_2.gd
@@ -1,6 +1,5 @@
 extends Area2D
 
-var test = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,13 +7,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	
 	pass
 
 
 func _on_body_entered(body: Node2D) -> void:
-	if test == 0:
-		test+= 1
-	#imi pare rau mihai, dar merge
-	else: 
-		get_tree().change_scene_to_file("res://Scenes/levels/village_2.tscn")
+	get_tree().change_scene_to_file("res://Scenes/levels/village_2.tscn")
 	pass # Replace with function body.
