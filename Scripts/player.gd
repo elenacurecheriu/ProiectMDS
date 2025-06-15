1b4a38a21aeb317916aab59329f58a5f1b87a3c2
elenacurecheriu <curecheriu.i.elena@gmail.com>
2025-06-15 14:57:50 +0300

    fixed spawning after death

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -104,7 +104,7 @@ func heal(amount):
 	
 	
 func die():
-	get_tree().change_scene_to_file("res://Scenes/levels/village.tscn")
+	get_tree().change_scene_to_file("res://Scenes/levels/village_3.tscn")
 	
 
 func _on_interaction_area_area_entered(area: Area2D) -> void:
